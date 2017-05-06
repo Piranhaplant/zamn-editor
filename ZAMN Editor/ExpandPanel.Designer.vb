e175479f2ef3b68b0d72e13edf604c15118ea898
Piranhaplant <piranhaplant1@gmail.com>
2017-05-06 11:16:06 -0500

    Fixed small bug with properties panel. Allow escape button to cancel save dialog.

--- ZAMN Editor/ExpandPanel.Designer.vb
+++ ZAMN Editor/ExpandPanel.Designer.vb
@@ -27,6 +27,9 @@ Partial Class ExpandPanel
         '
         'Panel1
         '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Panel1.Location = New System.Drawing.Point(0, 25)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(150, 125)
--- ZAMN Editor/SaveMsgBox.Designer.vb
+++ ZAMN Editor/SaveMsgBox.Designer.vb
@@ -74,6 +74,7 @@ Partial Class SaveMsgBox
         '
         'btnCancel
         '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnCancel.Location = New System.Drawing.Point(297, 284)
         Me.btnCancel.Name = "btnCancel"
@@ -96,6 +97,7 @@ Partial Class SaveMsgBox
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
         Me.ClientSize = New System.Drawing.Size(384, 320)
         Me.Controls.Add(Me.levelList)
         Me.Controls.Add(Me.btnCancel)
