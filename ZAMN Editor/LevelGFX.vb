92cb8dc999b3b49820f0d44927ac41c51880474d
Piranhaplant <piranhaplant1@gmail.com>
2018-01-07 14:22:05 -0600

    Cleaned up LevelGFX

--- ZAMN Editor/LevelGFX.vb
+++ ZAMN Editor/LevelGFX.vb
@@ -29,21 +29,17 @@
         Dim s2 As New IO.BinaryReader(New ByteArrayStream(My.Resources.VictimGFX))
         Do Until s2.BaseStream.Position >= s2.BaseStream.Length - 1
             Dim width As Integer = s2.ReadByte, height As Integer = s2.ReadByte
-            Dim img As New Bitmap(width * 8, height * 8)
+            Dim displayWidth As Integer = s2.ReadByte, displayHeight As Integer = s2.ReadByte
+            Dim img As New Bitmap(displayWidth * 8, displayHeight * 8)
             Dim pos As Integer = s2.BaseStream.Position
             For p As Byte = 0 To 1
                 For y As Integer = 0 To height - 1
                     For x As Integer = 0 To width - 1
                         Dim indx As Integer = s2.ReadInt32
                         If indx > 0 Then
-                            If s2.ReadByte = p Then
+                            If (s2.ReadByte <> 0) = (p <> 0) Then
                                 s.Seek(indx, IO.SeekOrigin.Begin)
                                 SNESGFX.DrawTile(img, x * 8 + s2.ReadSByte, y * 8 + s2.ReadSByte, s, plt, s2.ReadByte * 16, s2.ReadByte > 0, s2.ReadByte > 0)
-                                'Hardcoded override for army guys face palette
-                                If VictimImages.Count = 7 And y = 1 And (x = 0 Or x = 1) Then
-                                    DrawArmyOverride(s, plt)
-                                    If x = 1 Then ApplyOverride(img)
-                                End If
                             Else
                                 s2.BaseStream.Seek(5, IO.SeekOrigin.Current)
                             End If
@@ -52,46 +48,9 @@
                 Next
                 If p = 0 Then s2.BaseStream.Seek(pos, IO.SeekOrigin.Begin)
             Next
-            'TODO: Actually have the copy stored in VictimGFX.bin
-            If VictimImages.Count = 16 Then 'Second plant image
-                VictimImages.Add(img)
-            End If
             VictimImages.Add(img)
         Loop
-        'Crop Zeke image because it's an extra tile tall
-        VictimImages(40) = Crop(VictimImages(40), 16, 40)
         s.Close()
         s2.Close()
     End Sub
-
-    Private OverrideNum As Integer = 0
-    Private OverrideBmp As Bitmap = New Bitmap(16, 8)
-    Private NewPalette As Color()
-
-    Private Sub DrawArmyOverride(ByVal s As IO.FileStream, ByVal plt As Color())
-        If OverrideNum = 0 Then
-            NewPalette = plt.Clone()
-            NewPalette(20) = plt(4)
-            NewPalette(22) = plt(6)
-        End If
-        s.Seek(-&H20, IO.SeekOrigin.Current)
-        SNESGFX.DrawTile(OverrideBmp, OverrideNum * 8, 0, s, NewPalette, 16, False, False)
-        OverrideNum += 1
-    End Sub
-
-    Private Sub ApplyOverride(ByVal bmp As Bitmap)
-        Using g As Graphics = Graphics.FromImage(bmp)
-            g.DrawImage(OverrideBmp.Clone(New Rectangle(7, 1, 8, 5), OverrideBmp.PixelFormat), 7, 9, 8, 5)
-        End Using
-        OverrideNum = 0
-        OverrideBmp = New Bitmap(16, 8)
-    End Sub
-
-    Private Function Crop(ByVal img As Bitmap, ByVal width As Integer, ByVal height As Integer) As Bitmap
-        Dim newImg As New Bitmap(width, height, img.PixelFormat)
-        Using g As Graphics = Graphics.FromImage(newImg)
-            g.DrawImage(img, 0, 0)
-        End Using
-        Return newImg
-    End Function
 End Class
